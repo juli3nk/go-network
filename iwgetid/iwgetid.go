28dee7f74a3ffd09b464702b22fa6e0caafccb4b
Julien Kassar <code@juli3n.xyz>
2023-09-01 23:07:26 +0200

    Add iwgetid command

---
+++ iwgetid/iwgetid.go
@@ -0,0 +1,57 @@
+package iwgetid
+
+import (
+)
+
+func run(intf, flag string) (string, error) {
+	execPath, err := exec.LookPath("iwgetid")
+	if err != nil {
+		return "", err
+	}
+
+	out, err := exec.Command(execPath, intf, "-r", flag).Output()
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.TrimSpace(string(out)), nil
+}
+
+func GetSSID(intf string) (*string, error) {
+	ssid, err := run(intf, "-r")
+	if err != nil {
+		return nil, err
+	}
+
+	return &ssid, nil
+}
+
+func GetAccessPointMAC(intf string) (*string, error) {
+	apm, err = run(intf, "-a")
+	if err != nil {
+		return nil, err
+	}
+
+	return &apm, nil
+}
+
+func GetChannel(intf string) (int, error) {
+	ch, err := run(intf, "-c")
+	if err != nil {
+		return nil, err
+	}
+	chi, _ = strconv.Atoi(ch)
+
+    return chi, nil
+}
+
+func GetFrequency(intf string) (, error) {
+	freq, err := run(intf, "-f")
+	if err != nil {
+		return nil, err
+	}
+	freqFloat, _ := strconv.ParseFloat(freq, 64)
+	frequency = unit.Frequency(freqFloat) * unit.Hertz
+
+    return frequency, nil
+}
