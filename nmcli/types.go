9c4a39520f5a76c5ffcb8806193626f187bd9133
Julien Kassar <code@juli3n.xyz>
2025-02-27 08:48:54 +0100

    style(nmcli): improve struct field alignment for better readability

--- nmcli/types.go
+++ nmcli/types.go
@@ -1,27 +1,27 @@
 package nmcli
 
 type Device struct {
-	Name string
-	Type string
-	State string
+	Name       string
+	Type       string
+	State      string
 	Connection string
 }
 
 type Connection struct {
-	Name string
-	Uuid string
-	Type string
+	Name   string
+	Uuid   string
+	Type   string
 	Device string
 }
 
 type Wifi struct {
-	InUse bool `json:"in_use"`
-	Bssid string `json:"bssid"`
-	Ssid string `json:"ssid"`
-	Mode string `json:"mode"`
-	Chan int `json:"chan"`
-	Rate string `json:"rate"`
-	Signal int `json:"signal"`
-	Bars string `json:"bars"`
+	InUse    bool   `json:"in_use"`
+	Bssid    string `json:"bssid"`
+	Ssid     string `json:"ssid"`
+	Mode     string `json:"mode"`
+	Chan     int    `json:"chan"`
+	Rate     string `json:"rate"`
+	Signal   int    `json:"signal"`
+	Bars     string `json:"bars"`
 	Security string `json:"security"`
 }
